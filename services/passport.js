56039c6790b8294f265c3a68209021bf9438c7bc
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-16 18:56:47 -0400

    tweaked google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       let user = await User.findOne({ googleId: profile.id });
